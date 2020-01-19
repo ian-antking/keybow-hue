36cb497869d2a4a63a8d752361b2cb8e597355f5
Ian King <ian.antking@gmail.com>
2020-01-19 17:14:52 +0000

    reduce control key brightness

--- keybow-hue/app.py
+++ keybow-hue/app.py
@@ -5,7 +5,7 @@ import time
 control_keys = [0, 3, 6, 9]
 
 for key in control_keys:
-  keybow.set_led(key, 255, 255, 233)
+  keybow.set_led(key, 127, 127, 127)
 
 @keybow.on()
 def handle_key(index, state):
