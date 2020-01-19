1577c011f89389f6061ac2e972758eb8c2102a91
Ian King <ian.antking@gmail.com>
2020-01-19 16:22:01 +0000

    changed test script

--- keybow-hue/app.py
+++ keybow-hue/app.py
@@ -1,32 +1,21 @@
 #!/usr/bin/env python
 import keybow
 import time
-import colorsys
 
-colors = [
-    (255, 0, 0),
-    (0, 255, 0),
-    (0, 0, 255)
-]
 
-try:
-    for x in range(len(colors) * 2):
-        r, g, b = colors[0]
-        for x in range(12):
-            keybow.set_pixel(x, r, g, b)
-        colors.append(colors.pop(0))
-        keybow.show()
-        time.sleep(0.5)
+@keybow.on()
+def handle_key(index, state):
+    print("{}: Key {} has been {}".format(
+        time.time(),
+        index,
+        'pressed' if state else 'released'))
 
-    while True:
-        t = time.time()
-        for x in range(12):
-            h = t / 10.0
-            h += x / 12.0
-            r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
-            keybow.set_pixel(x, r, g, b)
-        keybow.show()
-        time.sleep(1.0 / 60)
+    if state:
+        keybow.set_led(index, 255, 0, 0)
+    else:
+        keybow.set_led(index, 0, 0, 0)
 
-except KeyboardInterrupt:
-    pass
+
+while True:
+    keybow.show()
+    time.sleep(1.0 / 60.0)
