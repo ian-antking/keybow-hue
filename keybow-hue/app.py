d4978424aa7d5f8056f5dac14e64e6114954d3e4
Ian King <ian.antking@gmail.com>
2020-01-20 14:16:36 +0000

    uncomment keybow import

--- keybow-hue/app.py
+++ keybow-hue/app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import keybow
 import time
+from shutdown import shutdown
 
 control_keys = [0, 3, 6, 9]
 
 for key in control_keys:
-  keybow.set_led(key, 127, 127, 127)
+    keybow.set_led(key, 127, 127, 127)
 
 @keybow.on()
 def handle_key(index, state):
@@ -20,6 +21,11 @@ def handle_key(index, state):
         keybow.set_led(index, 0, 0, 0)
 
 
-while True:
-    keybow.show()
-    time.sleep(1.0 / 60.0)
+if __name__ == '__main__':
+    killer = shutdown.Detector()
+    while not killer.kill_now:
+        keybow.show()
+        time.sleep(1.0 / 60.0)
+
+    print('Graceful Exit...')
+    exit()
