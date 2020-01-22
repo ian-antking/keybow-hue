e1470dfd94dc2f60b5c6e4c7085305f8f99fcc1d
Ian King <ian.antking@gmail.com>
2020-01-22 19:44:33 +0000

    remove print statement

--- keybow_hue/app.py
+++ keybow_hue/app.py
@@ -10,10 +10,6 @@ for key in control_keys:
 
 @keybow.on()
 def handle_key(index, state):
-    print("{}: Key {} has been {}".format(
-        time.time(),
-        index,
-        'pressed' if state else 'released'))
 
     if state:
         keybow.set_led(index, 0, 255, 0)
@@ -27,5 +23,4 @@ if __name__ == '__main__':
         keybow.show()
         time.sleep(1.0 / 60.0)
 
-    print('Graceful Exit...')
     exit()
