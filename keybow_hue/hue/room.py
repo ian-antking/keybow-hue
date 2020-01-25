52ff36d28207d946c78abeb3c6e4dddd3ed55108
Ian King <ian.antking@gmail.com>
2020-01-25 19:06:57 +0000

    room gets lights in constructor function

--- keybow_hue/hue/room.py
+++ keybow_hue/hue/room.py
@@ -1,12 +1,17 @@
 import requests
-
+import json
 
 class Room:
-    def __init__(self, hue_token, bridge_ip):
+    def __init__(self, hue_token, bridge_ip, room_name):
+        self.token = hue_token
+        self.bridge = bridge_ip
+        self.room_name = room_name
         self.url = f'http://{bridge_ip}/api/{hue_token}'
+        self.lights = self.get_lights()
     
-    def get_groups(self):
-        return requests.get(f'{self.url}/groups').content
+    def get_lights(self):
+        rooms = requests.get(f'{self.url}/groups').json()
+        return [room[1] for room in rooms.items() if room[1]['name'] == self.room_name][0]['lights']
 
 
 
@@ -16,8 +21,9 @@ if __name__ == '__main__':
     load_dotenv()
     hue_token = os.getenv('HUE_TOKEN')
     bridge_ip = os.getenv('BRIDGE_IP')
+    room_name = os.getenv('ROOM_NAME')
 
-    room = Room(hue_token, bridge_ip)
-    print(room.get_groups())
+    room = Room(hue_token, bridge_ip, room_name)
+    print(room.lights)
 
     
