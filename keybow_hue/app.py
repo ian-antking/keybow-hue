76821a7a4a50cfa75c8ad0416e16a56f2168da58
Ian King <ian.antking@gmail.com>
2020-01-26 21:04:11 +0000

    change position of brighten and dim buttons

--- keybow_hue/app.py
+++ keybow_hue/app.py
@@ -8,16 +8,16 @@ import os
 
 load_dotenv()
 
-dimmer_keys = [3, 6]
+dimmer_keys = [1, 2]
 
 @keybow.on()
 def handle_key(index, state):
 
     if state:
         keybow.set_led(index, 0, 255, 0)
-        if index == 3:
+        if index == 2:
             room.brighten()
-        if index == 6:
+        if index == 1:
             room.dim()
     else:
         keybow.set_led(index, room.brightness, room.brightness, room.brightness)
@@ -33,8 +33,8 @@ if __name__ == '__main__':
         print('No config')
     else:
         room = hue.Room(hue_token, bridge_ip, room_name)
-        keybow.set_led(6, room.brightness + 50, room.brightness + 50, room.brightness + 50)
-        keybow.set_led(3, room.brightness - 50, room.brightness - 50, room.brightness - 50)
+        keybow.set_led(2, room.brightness + 50, room.brightness + 50, room.brightness + 50)
+        keybow.set_led(1, room.brightness - 50, room.brightness - 50, room.brightness - 50)
 
     killer = shutdown.Detector()
     while not killer.kill_now:
