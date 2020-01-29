f5c57065179be5cabc5f8dc247e7e5f5c7ad47f5
Ian King <ian.antking@gmail.com>
2020-01-29 22:12:11 +0000

    instantiate keyboard object

--- keybow_hue/app.py
+++ keybow_hue/app.py
@@ -4,6 +4,7 @@ import shutdown
 import hue
 import time
 import config
+import interface
 from constants import DISCOVERY_URL
 
 @keybow.on()
@@ -11,42 +12,19 @@ def handle_key(index, state):
 
     if state:
         keybow.set_led(index, 0, 0, 255)
-        keys[index]['action']()
+        keyboard.keys[index]['action']()
     else:
         update_leds()
 
-
 def update_leds():
-    for key in keys:
-        keybow.set_led(key, *keys[key]['color']())
-
-
-def validate_brightness(brightness):
-    if brightness >= 255:
-        return 255
-    if brightness <= 0:
-        return 0
-    return brightness
-
+    for key in keyboard.keys:
+        keybow.set_led(key, *keyboard.keys[key]['color']())
 
 if __name__ == '__main__':
     try:
         conf = config.Env(['HUE_TOKEN', 'ROOM_NAME'])
         room = hue.Room(conf.env['ROOM_NAME'], hue.Bridge(conf.env['HUE_TOKEN'], DISCOVERY_URL))
-        keys = {
-            0: {
-                'action': room.dim,
-                'color': lambda: [validate_brightness(room.get_state('bri') - 50)] * 3 if room.get_state('on') else [0] * 3
-            },
-            1: {
-                'action': room.toggle_on_off,
-                'color': lambda: (0, 255, 0) if room.get_state('on') else (25, 0, 0)
-            },
-            2: {
-                'action': room.bright,
-                'color': lambda: [validate_brightness(room.get_state('bri') + 50)] * 3 if room.get_state('on') else [0] * 3
-            },
-        }
+        keyboard = interface.Keyboard(room)
         update_leds()
     except TypeError:
         keybow.set_all(255, 0, 0)
