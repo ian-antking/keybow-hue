a3f0e12ded63489ebfe78501f3244a20c5f87635
Ian King <ian.antking@gmail.com>
2020-01-29 22:31:15 +0000

    refactored app module into class

--- keybow_hue/app.py
+++ keybow_hue/app.py
@@ -7,27 +7,31 @@ import config
 import interface
 from constants import DISCOVERY_URL
 
-@keybow.on()
-def handle_key(index, state):
+class App():
+    def __init__(self, config_manager, hue_manager):
+        try:
+            conf = config.Env(['HUE_TOKEN', 'ROOM_NAME'])
+            self.room = hue.Room(conf.env['ROOM_NAME'], hue.Bridge(conf.env['HUE_TOKEN'], DISCOVERY_URL))
+            self.keyboard = interface.Keyboard(self.room)
+            self.update_leds()
+        except TypeError:
+            keybow.set_all(255, 0, 0)
 
-    if state:
-        keybow.set_led(index, 0, 0, 255)
-        keyboard.keys[index]['action']()
-    else:
-        update_leds()
-
-def update_leds():
-    for key in keyboard.keys:
-        keybow.set_led(key, *keyboard.keys[key]['color']())
+    def update_leds(self):
+        for key in self.keyboard.keys:
+            keybow.set_led(key, *self.keyboard.keys[key]['color']())
 
 if __name__ == '__main__':
-    try:
-        conf = config.Env(['HUE_TOKEN', 'ROOM_NAME'])
-        room = hue.Room(conf.env['ROOM_NAME'], hue.Bridge(conf.env['HUE_TOKEN'], DISCOVERY_URL))
-        keyboard = interface.Keyboard(room)
-        update_leds()
-    except TypeError:
-        keybow.set_all(255, 0, 0)
+    app = App(config, hue)
+
+    @keybow.on()
+    def handle_key(index, state):
+
+        if state:
+            keybow.set_led(index, 0, 0, 255)
+            app.keyboard.keys[index]['action']()
+        else:
+            app.update_leds()
 
     killer = shutdown.Detector()
     while not killer.kill_now:
