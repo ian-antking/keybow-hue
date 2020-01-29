9b8eb53af90dcbf4b02b22b57a6a0ee93873acb7
Ian King <ian.antking@gmail.com>
2020-01-29 17:55:56 +0000

    config.Env throws error if variable is None

--- keybow_hue/config/env.py
+++ keybow_hue/config/env.py
@@ -7,9 +7,12 @@ class Env:
     def __init__(self, args=[]):
         self.env = {}
         for arg in args:
-            self.env[arg] = os.getenv(arg)
+            env = os.getenv(arg)
+            if env == None:
+                raise TypeError(f'{arg} is not set')
+            self.env[arg] = env
 
 if __name__ == '__main__':
-    config_vars = ['HUE_TOKEN', 'ROOM_NAME']
+    config_vars = ['HUE_TOKEN', 'ROOM_NAMEs']
     conf = Env(config_vars)
     print(conf.env)
--- keybow_hue/config/tests/env_tests.py
+++ keybow_hue/config/tests/env_tests.py
@@ -1,11 +1,17 @@
-from nose.tools import assert_true
+from nose.tools import assert_true, assert_raises
 import config
 import os
 
-os.environ["HUE_TOKEN"] = "1234"
-os.environ["ROOM_NAME"] = "test_room"
+os.environ["TEST_TOKEN"] = "1234"
+os.environ["TEST_NAME"] = "test_room"
 
-def test():
-  env_vars = ['HUE_TOKEN', 'ROOM_NAME']
-  conf = config.Env(env_vars)
-  assert_true(conf.env['HUE_TOKEN'] == "1234" and conf.env['ROOM_NAME'] == "test_room")
+
+def test_Env_GivenVariableArray_LoadsVariablesFromOs():
+    env_vars = ['TEST_TOKEN', 'TEST_NAME']
+    conf = config.Env(env_vars)
+    assert_true(conf.env['TEST_TOKEN'] == "1234" and conf.env['TEST_NAME'] == "test_room")
+
+
+def test_Env_VariableNotInOs_ThrowsError():
+    env_vars = ['MISSING_VAR']
+    assert_raises(TypeError, config.Env, env_vars)
