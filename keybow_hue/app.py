e2853efcbb3b1c42231e8a02515870ef47e34aff
Ian King <ian.antking@gmail.com>
2020-01-29 23:20:17 +0000

    Revert "decoupled app and keybow"
    This reverts commit 593710c04add7f6efa91e985b804215eff38b44c.

--- keybow_hue/app.py
+++ keybow_hue/app.py
@@ -1,55 +1,37 @@
 #!/usr/bin/env python
+import keybow
+import shutdown
+import hue
+import time
+import config
+import interface
 from constants import DISCOVERY_URL
 
 class App():
-    def __init__(self, conf, hue_manager, state_manager):
-        room_name = conf.env['ROOM_NAME']
-        hue_token = conf.env['HUE_TOKEN'],
+    def __init__(self, config_manager, hue_manager):
+        try:
+            conf = config.Env(['HUE_TOKEN', 'ROOM_NAME'])
+            self.room = hue.Room(conf.env['ROOM_NAME'], hue.Bridge(conf.env['HUE_TOKEN'], DISCOVERY_URL))
+            self.keyboard = interface.Keyboard(self.room)
+            self.update_leds()
+        except TypeError:
+            keybow.set_all(255, 0, 0)
 
-        self.room = hue_manager.Room(
-            room_name,
-            hue_manager.Bridge(
-                hue_token,
-                DISCOVERY_URL
-            )
-        )
-        self.state = state_manager.Keyboard(self.room)
-
-    def update_leds(self, led_handler):
-        for key in self.state.keys:
-            led_handler(key, *self.state.keys[key]['color']())
-
-    def execute_action(self, index):
-        self.state.keys[index]['action']()
-
+    def update_leds(self):
+        for key in self.keyboard.keys:
+            keybow.set_led(key, *self.keyboard.keys[key]['color']())
 
 if __name__ == '__main__':
-    import keybow
-    import shutdown
-    import hue
-    import time
-    import config
-    import state
-
-    try: 
-        conf = config.Env(['HUE_TOKEN', 'ROOM_NAME'])
-    except TypeError:
-        keybow.set_all(255, 0, 0)
-        print(TypeError)
-        time.sleep(30)
-        exit()
-    
-    app = App(conf, hue, state)
-    app.update_leds(keybow.set_led)
+    app = App(config, hue)
 
     @keybow.on()
     def handle_key(index, state):
 
         if state:
             keybow.set_led(index, 0, 0, 255)
-            app.execute_action(index)
+            app.keyboard.keys[index]['action']()
         else:
-            app.update_leds(keybow.set_led)
+            app.update_leds()
 
     killer = shutdown.Detector()
     while not killer.kill_now:
