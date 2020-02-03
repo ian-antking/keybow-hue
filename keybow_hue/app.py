0598baeced58af00c3535d449c775fb843875565
Ian King <ian.antking@gmail.com>
2020-02-03 23:32:34 +0000

    fix incorrect dot notation

--- keybow_hue/app.py
+++ keybow_hue/app.py
@@ -27,9 +27,8 @@ class App():
 
     def update_leds(self):
         for key in self.state.keyboards[self.state.mode].keys:
-            color = self.validate_color(self.state.keyboards[self.state.mode].keys[key].color())
-            # self.led_controller.set_led(key.index, *color)
-            print(color)
+            color = self.validate_color(self.state.keyboards[self.state.mode].keys[key]['color']())
+            self.led_controller.set_led(key.index, *color)
 
     def execute_action(self, index):
         self.state.keyboards[self.state.mode][index].action()
