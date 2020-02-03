52596d2966dbfb7f8debd0d09eb92bdd68f6493a
Ian King <ian.antking@gmail.com>
2020-02-03 23:35:06 +0000

    fix key index

--- keybow_hue/app.py
+++ keybow_hue/app.py
@@ -28,7 +28,7 @@ class App():
     def update_leds(self):
         for key in self.state.keyboards[self.state.mode].keys:
             color = self.validate_color(self.state.keyboards[self.state.mode].keys[key]['color']())
-            self.led_controller.set_led(key.index, *color)
+            self.led_controller.set_led(key, *color)
 
     def execute_action(self, index):
         self.state.keyboards[self.state.mode][index].action()
