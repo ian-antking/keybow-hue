3ec8c69391e331b99ab9d17479420cbaebf86ad5
Ian King <ian.antking@gmail.com>
2020-02-05 13:52:22 +0000

    factor out color conversion to helper function

--- keybow_hue/helpers/keys.py
+++ keybow_hue/helpers/keys.py
@@ -1,5 +1,9 @@
 import colorsys
 
+def convert_hue_color(hue, saturaton, brightness):
+    color = colorsys.hsv_to_rgb(int(hue / 65535), saturaton / 254, brightness / 254)
+    return [value * 255 for value in color]
+
 def build_power_button(room, keyboard, key_index):
     return keyboard.Key(key_index, room.toggle_on_off, lambda: (0, 255, 0) if room.get_state('on') else (25, 0, 0))
 
@@ -10,7 +14,7 @@ def build_bright_button(room, keyboard, key_index):
     return keyboard.Key(key_index, room.bright, lambda: [room.get_state('bri') + 50] * 3 if room.get_state('on') else [0] * 3)
 
 def build_increase_hue_button(room, keyboard, key_index):
-    return keyboard.Key(key_index, room.increase_hue, lambda: colorsys.hsv_to_rgb(360 * (int((room.get_state('hue') + 6553) / 65535)), int((room.get_state('sat') / 254) * 100), int((room.get_state('bri') / 254) * 100)))
+    return keyboard.Key(key_index, room.increase_hue, lambda:(convert_hue_color(room.get_state('hue') + 6553, 254, 254)))
 
 def build_decrease_hue_button(room, keyboard, key_index):
-    return keyboard.Key(key_index, room.decrease_hue, lambda: colorsys.hsv_to_rgb(360 * (int((room.get_state('hue') - 6553) / 65535)), int((room.get_state('sat') / 254) * 100), int((room.get_state('bri') / 254) * 100)))
+    return keyboard.Key(key_index, room.decrease_hue, lambda:(convert_hue_color(room.get_state('hue') - 6553, 254, 254)))
